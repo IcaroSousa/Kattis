ffcdfe85b61b57c322968ac65a8597c57d6509e7
Icaro Ferreira <icaro.ferreira@softplan.com.br>
2018-03-02 11:00:17 -0300

    #Renamed Solution; #Added CalculateAreaFromPoint [Hull] project;

---
+++ Kattis.CookieStore/Kattis.AreaFromPoints/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Kattis.AreaFromPoints
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
